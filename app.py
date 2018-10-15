d8141fd6deb49a4649575fbe0b177a66bb79cfeb
kamiko <b17016@kaetsu.ac.jp>
2018-10-15 11:50:07 +0900

    change app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def post():
         access_token_key="1049129656379535360-LkXoFhHwr56IEH4TKS0LiE1sTK6VOj",
         access_token_secret="epwTxvBOiqijuDyeuyBdsRk8KyY8JA8PzGpVOD6jLRBIv"
         )
-    api.PostUpdate("Flaskから3回めの投稿 ({datetime()})")
+    api.PostUpdate("Flaskから3回めの投稿" + str(datetime.now()))
 
     return render_template('result.html')
 
