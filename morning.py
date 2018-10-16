bd192e5b41a3cd21af161317fef7cc79c14d1c45
kamiko <b17016@kaetsu.ac.jp>
2018-10-16 10:36:58 +0900

    add api to morning.py

--- morning.py
+++ morning.py
@@ -4,6 +4,12 @@
 import os
 from datetime import datetime
 
+api = twitter.Api(consumer_key="U84inIJFauv3RUFedHOwzPGLs",
+    consumer_secret="VtbtEHaQz2hV3CTachsa29R4JOsLbVkTpxUoTbuSaPmSm5vhOa",
+    access_token_key="1049129656379535360-LkXoFhHwr56IEH4TKS0LiE1sTK6VOj",
+    access_token_secret="epwTxvBOiqijuDyeuyBdsRk8KyY8JA8PzGpVOD6jLRBIv"
+    )
+
 # 「起きて！！！」とTwitterに投稿する
 def morning():
     morning_call = "起きて！" + "\n「" + str(datetime.now()) + "」だよ！！！"
