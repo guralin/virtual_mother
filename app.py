c204af28d28d4f4bac3abeddf9e7e216d0135d5e
kamiko <b17016@kaetsu.ac.jp>
2018-10-22 01:25:30 +0900

    moduleファイルを作りました

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
 
-from index import Index
-from post import Post
+from module import index
+from module import post
 
 
 # 投稿する
 @app.route('/')
 def index_do():
-    do = Index()
+    do = index.Index()
     return render_template('index.html')
 
 # 投稿結果
 @app.route('/post')
 def post_do():
-    do = Post()
+    do = post.Post()
     return render_template('post.html', post_text=do.post_text())
 
 """
--- module/index.py
+++ module/index.py
@@ -4,3 +4,4 @@
 class Index():
     def __init__(self):
         pass
+
