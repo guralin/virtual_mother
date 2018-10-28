586155c7faf23e7334adf469fde721d486768121
K1-scuba_diving <40312446+guralin@users.noreply.github.com>
2018-10-28 17:48:15 +0900

    Revert "Test database create"

--- app.py
+++ app.py
@@ -2,10 +2,9 @@
 # coding: utf-8
 
 import os
-from flask import Flask, render_template, request,g, redirect, url_for
+from flask import Flask, render_template, request
 app = Flask(__name__)
 app.debug = True
-from flask_sqlalchemy import SQLAlchemy
 
 from module import index
 from module import post
@@ -31,20 +30,6 @@ class User(db.Model):
 ######################
 
 
-db_uri = os.environ.get('DATABASE_URL') or "postgresql:///flasknote"# 追加
-#db_uri = "postgresql:///flasknote"# 追加
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = db_uri # 追加
-db = SQLAlchemy(app) # 追加
-
-
-class Entry(db.Model): # 追加
-    __tablename__ = "entries" # 追加
-    id = db.Column(db.Integer, primary_key=True) # 追加
-    title = db.Column(db.String(), nullable=False) # 追加
-    body = db.Column(db.String(), nullable=False) # 追加
-
-
 # 投稿する
 @app.route('/')
 def do_index():
@@ -64,33 +49,6 @@ def do_reply():
     if request.method == 'POST':
         result = request.form
     reply_name = result["reply_name"] 
-
-    post_text = do.send_reply(reply_name)
-    return render_template('post.html',post_text=post_text)
-
-@app.route('/dbtest')
-def hello_world():
-    entries = Entry.query.all()
-    return render_template('dbtest.html', entries=entries)
-
-
-# Database
-def connect_db():
-    db_path = os.path.join(app.root_path, 'flasknote.db')
-    rv = sqlite3.connect(db_path)
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
     return render_template('post.html',post_text=do.send_reply(reply_name))
 
 ##########################
@@ -118,14 +76,6 @@ def hello(name=''):
 def debug():
     return render_template('notemplate.html')
 
-@app.route('/dbpost', methods=['POST'])
-def add_entry():
-    entry = Entry()
-    entry.title = request.form['title']
-    entry.body = request.form['body']
-    db.session.add(entry)
-    db.session.commit()
-    return redirect(url_for('hello_world'))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
