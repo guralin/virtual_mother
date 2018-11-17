4a98ffc29b3811ae2806acd92c9aad841a4df7fe
kamiko <b17016@kaetsu.ac.jp>
2018-11-17 09:01:34 +0900

    passしてみる

--- app.py
+++ app.py
@@ -81,19 +81,21 @@ def check_token():
     oauth_verifier = request.args.get('oauth_verifier', default = "failed", type = str)
 
     if oauth_token != "failed" and oauth_verifier !="failed":
+        pass
+"""
         logging.debug("oauth_token and oauth_verifier is not failed")
         response = get_access_token(oauth_token, oauth_verifier).decode('utf-8')
         response = dict(parse_qsl(response))
         oauth_token = response['oauth_token']
         oauth_token_secret = response['oauth_token_secret']
-        return render_template('cer.html',url="NoNeed") #,oauth_token=oauth_token,oauth_token_secret=oauth_token_secret)
+        return render_template('cer.html',url="NoNeed")
+"""
     else:
         logging.debug("oauth_token or oauth_verifier is failed")
         #リクエストトークンを取得する
         request_token = get_request_token()
         authorize_url = '%s?oauth_token=%s' % (authenticate_url, request_token)
         logging.debug(authorize_url)
-#        return render_template('cer.html',url=authorize_url,res="NoParams")
         return redirect(authorize_url)
 
 # index
