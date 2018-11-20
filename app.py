1ad63c6eab4e8e3f13a4ac4bb3edd99cae8daf2a
home_ubu18_gura <tellurium38@gmail.com>
2018-11-20 22:52:24 +0900

    import tweet

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 import logging
 
 import oauth2 as oauth
+from module import tweet
 
 from flask import Flask, render_template, request, jsonify, redirect
 from flask_sqlalchemy import SQLAlchemy
