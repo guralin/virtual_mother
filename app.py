fd8f9e9045f1d1f7ada7df2d623138cc0819dfb3
kamiko <b17016@kaetsu.ac.jp>
2018-11-24 02:29:58 +0900

    views.pyの作成

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 #!/bin/env python
 # coding: utf-8
 
+# appの実行
+
+
+
 import os
 
 from virtualmother_app import app
 
+
+
+app.debug = True
+
 port = int(os.environ.get('PORT', 5000))
 app.run(port=port)
+
+
+
