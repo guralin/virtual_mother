fdc4e55721de3f29d119b8fb180aac3f33f6ee6f
gur <tellurium38@gmail.com>
2018-11-26 15:42:25 +0900

    １０分おきに該当するユーザーにリプライ

--- test.py
+++ test.py
@@ -2,11 +2,13 @@ from virtualmother_app.module  import database,tweet
 from virtualmother_app import db
 from datetime import time
 
-do = database.DBOperation(db)
+db = database.DBOperation(db)
 
-do.update_get_up_time('1049129656379535360',time(22,30))
 
+#db.update_get_up_time('1049129656379535360',time(12,30))
 
 do = tweet.MothersTwitter()
 
-print(do.id_for_screen_name("1049129656379535360"))
+#do.call_screen_name("1049129656379535360")
+
+print(do.call_screen_name("111111"))
