abfcdcd130a4405b3b06e89d3138f56afd4c871a
kamiko <b17016@kaetsu.ac.jp>
2018-12-06 00:23:03 +0900

    ozisanフォルダの削除

--- ozisan/o.py
+++
@@ -1,9 +0,0 @@
-class Person():
-    def __init__(a,name):
-        a.name = name
-
-    def ohayo(a):
-        return a.name
-class mikio(Person):
-    def sayonara(a):
-        return range(len(a.name))
