b226989a656c2d93a9ce9cab605417118495321c
kamiko <b17016@kaetsu.ac.jp>
2018-12-07 17:58:45 +0900

    日付の登録

--- virtualmother_app/module/database.py
+++ virtualmother_app/module/database.py
@@ -17,6 +17,13 @@ class SendData(Table):
 
 
 
+class SendDate(Table):
+    def __init__(self, user_id, date):
+        self.user_id     = user_id
+        self.date = date
+
+
+
 class DBOperation():
 
     def __init__(self,db):
@@ -30,7 +37,7 @@ class DBOperation():
 
     # 日付を登録
     def insert_date(self, user_id):
-        user_data = SendData(user_id, datetime.date.today())
+        user_data = SendDate(user_id, datetime.date.today())
         db.session.add(user_data)
         db.session.commit()
 
@@ -39,6 +46,35 @@ class DBOperation():
         user_data = db.session.query(Table).filter(Table.user_id==user_id).first()
         user_data.get_up_time = get_up_time
         db.session.commit()
+
+    # 日付の更新（DMのリンクをクリックした時）
+    def update_date(self, user_id):
+        user_data = db.session.query(Table).filter(Table.user_id==user_id).first()
+        user_data.date = datetime.date.today() #(1600, 2, 4)
+        print(user_data.date)
+        db.session.commit()
+
+    # 目覚まし解除
+    def delete_get_up_time(self, user_id):
+        user_data = db.session.query(Table).filter(Table.user_id==user_id).first()
+        db.session.delete(user_data)
+        db.session.commit()
+
+
+
+# morning.py
+class GetData(Table): # カラムを指定してデータを取得
+    def id_and_get_up(self):
+        users = db.session.query(Table.user_id,Table.get_up_time).all()
+        return users
+    
+    
+
+    def __repr__(self):
+        return self.user_id
+
+
+
 
     # 日付の更新（DMのリンクをクリックした時）
     def update_date(self, user_id):
